f5cfcda5ba4ae2e72b6fb86afb1910b455f1c0c6
Tobias Fieser <tobiasfieser@googlemail.com>
2025-09-03 21:36:21 +0200

    -Added Server Class

---
+++ BlazorTest/BlazorTest/Models/Server.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorTest.Models
+{
+    public class Server
+    {
+
+        public int ServerID { get; set; }
+        public bool IsOnline { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+
+        public Server()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0,2);
+            IsOnline = randomNumber == 0? false : true;
+        }
+
+    }
+}
