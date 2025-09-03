efcbc394807138de0f49fc8b8a33a217df4bf90c
Tobias Fieser <tobiasfieser@googlemail.com>
2025-09-03 22:01:16 +0200

    -Added ServerRepository

---
+++ BlazorTest/BlazorTest/Models/ServerRepository.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography;
+using System.Xml.Schema;
+
+namespace BlazorTest.Models
+{
+    public static class ServerRepository
+    {
+
+        private static List<Server> _servers = new List<Server>
+        {
+            new Server{ Name = "Server_1", City = "Karlsruhe", ServerID = 1},
+            new Server{ Name = "Server_2", City = "Berlin", ServerID = 2},
+            new Server{ Name = "Server_3", City = "Frankfurt", ServerID = 3},
+            new Server{ Name = "Server_4", City = "Stuttgart", ServerID = 4},
+            new Server{ Name = "Server_5", City = "Hamburg", ServerID = 5},
+            new Server{ Name = "Server_6", City = "Dortmund", ServerID = 6},
+            new Server{ Name = "Server_7", City = "Freibrug", ServerID = 7},
+            new Server{ Name = "Server_8", City = "New York", ServerID = 8},
+        };
+
+        public static void AddServer(Server server)
+        {
+            var maxID = _servers.Max(s => s.ServerID);
+            server.ServerID = maxID++;
+            _servers.Add(server);
+        }
+
+        public static List<Server> GetServers()
+            => _servers;
+
+        public static List<Server> GetServerByCity(string city)
+            => _servers.Where(s => s.City.Equals(city)).ToList();
+
+        public static Server GetServerByID(int id)
+        {
+            var server = _servers.FirstOrDefault(s => s.ServerID == id);
+
+            if (server == null) 
+                return null;
+
+            Server s1 = new Server
+            {
+                ServerID = id,
+                Name = _servers[id].Name,
+                City = _servers[id].City,
+                IsOnline = _servers[id].IsOnline,
+            };
+
+            return s1;
+        }
+
+        public static void DeleteServer(int id)
+        {
+            var server = _servers.FirstOrDefault(s => s.ServerID == id);
+
+            if (server == null) return;
+
+            _servers.Remove(server);
+        }
+
+        public static List<Server> FindServer(string searchString)
+            => _servers.Where(s1 => s1.Name.Contains(searchString)).ToList();
+    }
+}
--- BlazorTest/BlazorTest/obj/Debug/net9.0/BlazorTest.AssemblyInfo.cs
+++ BlazorTest/BlazorTest/obj/Debug/net9.0/BlazorTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e111e66499e21697dc7fd8029dfddcca0b1d7f74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5cfcda5ba4ae2e72b6fb86afb1910b455f1c0c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
