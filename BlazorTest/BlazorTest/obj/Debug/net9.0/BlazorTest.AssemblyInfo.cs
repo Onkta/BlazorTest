fc02f07c608be4c7e7b4a9dfdab8eb02daf26197
Tobias Fieser <tobiasfieser@googlemail.com>
2025-09-04 11:27:48 +0200

    Letzter Stand vor pause

--- BlazorTest/BlazorTest/obj/Debug/net9.0/BlazorTest.AssemblyInfo.cs
+++ BlazorTest/BlazorTest/obj/Debug/net9.0/BlazorTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1556218eae1475dd072a5b8e89b2125f2b7bc0a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04dba1de80cb78cbeb60c5a9856f98b08263dc24")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
