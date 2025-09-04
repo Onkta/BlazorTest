4399ea4f7c74a08bb9ea8b7ee58a776f49465105
Tobias Fieser <tobiasfieser@googlemail.com>
2025-09-04 11:51:11 +0200

    update gitignore

--- BlazorTest/BlazorTest/obj/Debug/net9.0/BlazorTest.AssemblyInfo.cs
+++ BlazorTest/BlazorTest/obj/Debug/net9.0/BlazorTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04dba1de80cb78cbeb60c5a9856f98b08263dc24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc02f07c608be4c7e7b4a9dfdab8eb02daf26197")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
