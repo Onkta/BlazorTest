141e8c634174f9847313be11a4f5a2fddfa54245
Tobias Fieser <tobiasfieser@googlemail.com>
2025-09-04 12:43:37 +0200

    added data annotations

--- BlazorTest/BlazorTest/Models/Server.cs
+++ BlazorTest/BlazorTest/Models/Server.cs
@@ -1,11 +1,18 @@
-﻿namespace BlazorTest.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTest.Models
 {
     public class Server
     {
 
         public int ServerID { get; set; }
         public bool IsOnline { get; set; }
+
+        [Required]
+        [StringLength(20,ErrorMessage = "Name not longer than 20 char")]
         public string? Name { get; set; }
+
+        [Required]
         public string? City { get; set; }
 
         public Server()
